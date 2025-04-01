98c0eabf44109fc59a7773adc5463900078c7d8a
biaag10 <107511894+biaag10@users.noreply.github.com>
2025-04-01 16:35:37 -0300

    ajustes em projetos, no header e em habilidades

---
+++ script.js
@@ -0,0 +1,13 @@
+// // seleciona todos os flashcards
+// const flashcards = document.querySelectorAll('.flashcard');
+
+// // adicionando o evento de hover para cada flashcard
+// flashcards.forEach(card => {
+//     card.addEventListener('mouseenter', function() {
+//         card.classList.add('flipped');  // adiciona a classe para virar o card
+//     });
+
+//     card.addEventListener('mouseleave', function() {
+//         card.classList.remove('flipped'); // remove a classe para voltar ao estado normal
+//     });
+// });
