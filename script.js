271bf4a3ff2c9c8a82056ffc39558e7756697826
biaag10 <107511894+biaag10@users.noreply.github.com>
2025-04-01 18:03:53 -0300

    efeito suave no scroll

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// // seleciona todos os flashcards
-// const flashcards = document.querySelectorAll('.flashcard');
+// scroll Suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
-// // adicionando o evento de hover para cada flashcard
-// flashcards.forEach(card => {
-//     card.addEventListener('mouseenter', function() {
-//         card.classList.add('flipped');  // adiciona a classe para virar o card
-//     });
 
-//     card.addEventListener('mouseleave', function() {
-//         card.classList.remove('flipped'); // remove a classe para voltar ao estado normal
-//     });
-// });
